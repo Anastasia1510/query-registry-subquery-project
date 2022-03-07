c178e8308d83336e3f33724ccba03d8b890ccd05
Scott Twiname <skott.twiname@gmail.com>
2022-03-07 14:30:42 +1300

    Update contracts-sdk, retain rewards

--- src/mappings/rewardsDistributor.ts
+++ src/mappings/rewardsDistributor.ts
@@ -2,51 +2,69 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import assert from 'assert';
-import { MoonbeamEvent } from "@subql/contract-processors/dist/moonbeam";
+import { MoonbeamEvent } from '@subql/contract-processors/dist/moonbeam';
 import { Delegation, Reward } from '../types';
 import { RewardsDistributer__factory } from '@subql/contract-sdk';
 import FrontierEthProvider from './ethProvider';
-import { ClaimRewardsEvent, DistributeRewardsEvent } from '@subql/contract-sdk/typechain/RewardsDistributer';
+import {
+  ClaimRewardsEvent,
+  DistributeRewardsEvent,
+} from '@subql/contract-sdk/typechain/RewardsDistributer';
 
 function buildRewardId(indexer: string, delegator: string): string {
   return `${indexer}:${delegator}`;
 }
 
-export async function handleRewardsDistributed(event: MoonbeamEvent<DistributeRewardsEvent['args']>): Promise<void> {
+export async function handleRewardsDistributed(
+  event: MoonbeamEvent<DistributeRewardsEvent['args']>
+): Promise<void> {
   assert(event.args, 'No event args');
 
   const { indexer } = event.args;
   const delegators = await Delegation.getByIndexerId(indexer);
   if (!delegators) return;
 
+  const rewardsDistributor = RewardsDistributer__factory.connect(
+    '',
+    new FrontierEthProvider()
+  );
 
-  const rewardsDistributor = RewardsDistributer__factory.connect('', new FrontierEthProvider());
+  await Promise.all(
+    delegators.map(async (delegator) => {
+      const rewards = await rewardsDistributor.userRewards(
+        indexer,
+        delegator.id
+      );
+      const id = buildRewardId(indexer, delegator.id);
 
-  Promise.all(delegators.map(async delegator => {
+      let reward = await Reward.get(id);
 
-    const rewards = await rewardsDistributor.userRewards(indexer, delegator.id);
-    const id = buildRewardId(indexer, delegator.id);
+      if (!reward) {
+        reward = Reward.create({
+          id,
+          delegatorAddress: delegator.id,
+          indexerAddress: indexer,
+          amount: rewards.toBigInt(),
+          claimed: false,
+        });
+      }
 
-    let reward = await Reward.get(id);
-
-    if (!reward) {
-      reward = Reward.create({
-        id,
-        delegatorAddress: delegator.id,
-        indexerAddress: indexer,
-        amount: rewards.toBigInt(),
-      });
-    }
-
-    await reward.save();
-  }));
+      await reward.save();
+    })
+  );
 }
 
-export async function handleRewardsClaimed(event: MoonbeamEvent<ClaimRewardsEvent['args']>): Promise<void> {
+export async function handleRewardsClaimed(
+  event: MoonbeamEvent<ClaimRewardsEvent['args']>
+): Promise<void> {
   assert(event.args, 'No event args');
 
   const id = buildRewardId(event.args.indexer, event.args.delegator);
 
-  await Reward.remove(id);
-}
+  const reward = await Reward.get(id);
+  assert(reward, `Reward not found: ${id}`);
 
+  reward.claimed = true;
+
+  await reward.save();
+}
